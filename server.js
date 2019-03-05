0524df74e70aa52c6d971f672b53c9bfd2fe3010
ScottyKnows44 <Scottcfalcon@gmail.com>
2019-03-04 17:36:02 -0800

    changes to server.js

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 `use strict`;
 
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const pg = require('pg');
+
+const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
